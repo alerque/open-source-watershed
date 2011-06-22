79eadb495cef55fadf6cde0a764ff59e1f107de0
Caleb Maclennan <caleb@alerque.com>
2011-06-23 00:25:37 +0300

    Began adding distro crawler for PLD-Linux.

--- crawl/distros/pld.py
+++ crawl/distros/pld.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import time
+import datetime
+import os
+from .utils import helper
+from .utils.db import downstream
+from .utils.types import Repo, DownstreamRelease
+
 MIRROR = "ftp.pld-linux.org"
 HTTP_START_DIR = None
 FTP_START_DIR = None
 
-ARCHES = ["i686", "x86_64"]
+ARCHES = ["i686","x86_64"]
 
 distro_id = downstream.distro("pld", "", "A rolling release binay distribution.", "http://pld-linux.org")
 
 # return a list of ["ubuntu", branch, codename, component, arch, None, None]
-def get_repos():
+def get_repos(test):
 	repos = []
 	for comp in ["ac", "th", "ti"]:
 		for a in ARCHES:
@@ -21,17 +30,18 @@ def get_repos():
 			downstream.add_branch(repo, "current", test)
 	
 			for comp-tree in ["ready", "test"]:
-					repo = Repo()
-					repo.distro_id = distro_id
-					repo.component = comp + "-" + comptree
-					repo.architecture = a
-					repo.codename = ""
-					repos.append(repo)
-					downstream.repo(repo, test)
-					downstream.add_branch(repo, "future", test)
+				repo = Repo()
+				repo.distro_id = distro_id
+				repo.component = comp + "-" + comptree
+				repo.architecture = a
+				repo.codename = ""
+				repos.append(repo)
+				downstream.repo(repo, test)
+				downstream.add_branch(repo, "future", test)
 	return repos
 
 # return a list of [name, version, revision, epoch, time, extra]
 def crawl_repo(repo):
+	rels = []
   distro,branch,codename,component,arch,last_crawl,new = repo
   pass
