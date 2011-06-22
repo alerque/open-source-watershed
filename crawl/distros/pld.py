50527a66a6cbd168fc43919eac5bc4f83a85c6d5
Caleb Maclennan <caleb@alerque.com>
2011-06-23 00:21:14 +0300

    Began creating distro file to include PLD-Linux support.

---
+++ crawl/distros/pld.py
@@ -0,0 +1,37 @@
+MIRROR = "ftp.pld-linux.org"
+HTTP_START_DIR = None
+FTP_START_DIR = None
+
+ARCHES = ["i686", "x86_64"]
+
+distro_id = downstream.distro("pld", "", "A rolling release binay distribution.", "http://pld-linux.org")
+
+# return a list of ["ubuntu", branch, codename, component, arch, None, None]
+def get_repos():
+	repos = []
+	for comp in ["ac", "th", "ti"]:
+		for a in ARCHES:
+			repo = Repo()
+			repo.distro_id = distro_id
+			repo.component = comp
+			repo.architecture = a
+			repo.codename = ""
+			repos.append(repo)
+			downstream.repo(repo, test)
+			downstream.add_branch(repo, "current", test)
+	
+			for comp-tree in ["ready", "test"]:
+					repo = Repo()
+					repo.distro_id = distro_id
+					repo.component = comp + "-" + comptree
+					repo.architecture = a
+					repo.codename = ""
+					repos.append(repo)
+					downstream.repo(repo, test)
+					downstream.add_branch(repo, "future", test)
+	return repos
+
+# return a list of [name, version, revision, epoch, time, extra]
+def crawl_repo(repo):
+  distro,branch,codename,component,arch,last_crawl,new = repo
+  pass
